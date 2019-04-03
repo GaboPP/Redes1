763737f042ebb52dfb7e41a3bd1e34ffcc1ee0b5
LAPTOP-2VCBBLJO\gabriel <gabriel.astorga@sansano.usm.cl>
2019-04-03 14:10:28 -0300

    last gabriel

---
+++ src/Cliente/client.java
@@ -0,0 +1,82 @@
+package Cliente;
+
+import java.io.*;
+import java.net.*;;
+import org.json.*;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+
+//        if (args.length != 2) {
+//            System.err.println(
+//                    "Usage: java EchoClient <host name> <port number>");
+//            System.exit(1);
+//        }
+
+        String hostName =  "0.0.0.0"; //args[0];
+        int portNumber = 4444; //Integer.parseInt(args[1]);
+
+        try (
+                Socket kkSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                    new BufferedReader(new InputStreamReader(System.in));
+            JSONObject fromServer;
+            String fromUser;
+            JSONArray response;
+            String message;
+            String file_line;
+
+            try {
+                fromServer = new JSONObject(in.readLine());
+                while (!fromServer.getString("message").equals("Bye.")) {
+                    if (fromServer.opt("response") != null) {
+                            response = fromServer.getJSONArray("response");
+                        System.out.println("Server: " + response);
+                    }
+                    message = fromServer.getString("message");
+                    System.out.println("Server: " + message);
+
+//                    if (fromServer.equals("Bye."))
+//                        break;
+
+                    fromUser = stdIn.readLine();
+                    if (fromUser != null) {
+                        System.out.println("Client: " + fromUser);
+                        out.println(fromUser);
+                        if(fromUser.split(" ")[0] == "get"){ //si el cliente envia get
+
+                            String file = fromUser.split(" ")[1];  //obtengo el nombre del archivo
+                            BufferedWriter writer = null;
+                            while (!fromServer.getString("check").equals("Done.")) {  //copio proceso de lectura desde el server para capturar cada linea del archivo
+                                if (fromServer.opt("response") != null) {
+                                    response = fromServer.getJSONArray("line_file");
+                                    System.out.println("Server: " + response);
+
+                                    file_line = fromServer.getString("line_file");
+                                    System.out.println("Servertxt: " +  file_line);
+                                    writer = new BufferedWriter(new FileWriter(file));  //creo archivo y lo dejo listo para escribir
+                                    writer.write(file_line); //escribo linea
+                                }
+                            }
+                            writer.close();  //cierro archivo o quizas puntero, no estoy seguro xD
+                        }
+                    }
+                    fromServer = new JSONObject(in.readLine());
+            }} catch(JSONException e) {
+                System.out.println((e));
+                e.getCause();
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
---
+++ src/Servidor/multiserverthread.java
@@ -0,0 +1,41 @@
+package Servidor;
+
+import java.net.*;
+import java.io.*;
+
+public class multiserverthread implements Runnable {
+    private Socket socket = null;
+
+    public multiserverthread(Socket socket) {
+//        super("multiserverthread");
+        this.socket = socket;
+        System.out.println(this.socket.getRemoteSocketAddress().toString() + " Conexión entrante");
+    }
+
+    public void run() {
+
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+        ) {
+            String inputLine;
+            Object outputLine;
+            protocol protocolo = new protocol();
+            outputLine = protocolo.processInput(null,null);
+            out.println(outputLine);
+            //System.out.println("Esto llega: " + in.readLine());
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine.split(" ")[0]);
+                outputLine = protocolo.processInput(inputLine, socket);
+                out.println(outputLine);
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
