1e084740571b44d210236ddbb10f26abc8b051b3
chrisgcs <christopher.gilbert@sansano.usm.cl>
2019-05-19 21:27:31 -0400

    Codigo de las maquinas

---
+++ src/Maquina/README.txt
@@ -0,0 +1,43 @@
+## Development
+
+##Nombres
+Christopher Gilbert Carroza 201573597-2
+Gabriel Astorga Baeza 201573591-3
+
+Para la ejecución de la tarea se debe abrir un terminal en la carpeta actual y escribir el comando
+
+	make
+
+Para la compilación del código perteneciente al servidor.
+
+Luego se debe iniciar el servidor con el comando:
+
+	make run
+
+Para la compilación y ejecución del cliente, basta abrir otro terminal en esta carpeta y escribir en consola:
+
+	make cliente
+
+Y procedera a dar inicio al cliente del FTP
+
+######
+	Observaciones:
+
+Al momento de ejecutar el cliente se solicitara la dirección IP en la cual se encuentra el servidor.
+
+Luego de ingresar la IP se solicitara usuario y contraseña
+				
+		###		IMPORTANTE.          ###
+
+El formato del usuario y contraseña es el siguiente
+
+			usuario/contraseña
+Ejemplo:
+
+	user/pass
+
+Cualquier otra forma de escribirlo resultará en error y deberá ejecutarse nuevamente el cliente. 
+
+Los pares (usuario, contraseña) es posible visualizarlos en el archivo de texto
+
+		"login.txt"
---
+++ src/Maquina/src/Servidor/multiserverthread.java
@@ -0,0 +1,52 @@
+package Servidor;
+
+import java.net.*;
+import java.io.*;
+
+public class multiserverthread extends Thread {
+    private Socket socket = null;
+    private FileWriter logs;
+
+    int portNumberV1 = 4448;
+    int portNumberV2 = 4449;
+    String hostNameV1 = "10.6.40.183"; // Maquina 43
+    String hostNameV2 = "10.6.40.184"; // Maquina 44
+    boolean connectionVM = true;
+
+    public multiserverthread(Socket socket, FileWriter logs) {
+        // super("multiserverthread");
+        this.socket = socket;
+        this.logs = logs;
+        System.out.println(this.socket.getRemoteSocketAddress().toString() + " Conexion entrante");
+    }
+
+    public void run() {
+
+        try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+            String inputLine;
+            Object outputLine;
+            protocol protocolo = new protocol();
+
+
+
+
+            outputLine = protocolo.processInput(null, null, logs);
+            out.println(outputLine);
+            // System.out.println("Esto llega: " + in.readLine());
+            
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine.split(" ")[0]);
+                outputLine = protocolo.processInput(inputLine, socket, logs);
+                out.println(outputLine);
+
+
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
